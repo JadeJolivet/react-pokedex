1c1a49a93c8bd2fd10150b140090f9d74194e0f8
Jade Jolivet <jade.jolivet@gmail.com>
2024-03-27 16:36:35 +0100

    Props pokemonCard

--- React-pokedex/src/App.jsx
+++ React-pokedex/src/App.jsx
@@ -4,11 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from "./components/PokemonCard"
 
-function App() {
+const App = () => {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
-    <div>
-      <PokemonCard/>
-    </div>
+    <>
+      <PokemonCard pokemon={pokemonList[1]} />
+    </>
   );
 }
 
--- React-pokedex/src/components/PokemonCard.jsx
+++ React-pokedex/src/components/PokemonCard.jsx
@@ -1,7 +1,5 @@
-function PokemonCard() {
-   
-    const pokemon = pokemonList[1];
-    
+function PokemonCard ({pokemon}){ 
+
     return (
       <figure>
         {pokemon.imgSrc ? (
@@ -18,13 +16,5 @@ function PokemonCard() {
   
   export default PokemonCard;
 
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+
+  
